1f9854ef897d5a952f23bbfc35dca941eb0062b6
rikves <skosalah1@gmail.com>
2025-07-08 20:16:38 +0100

    33%

---
+++ C01/ex02/how.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+// Pass by value is literally different from Pass by reference here is how
+void borken_swap(int a, int b){
+	printf("BEFORE\n");
+	printf("%d\n",a);
+	printf("%d\n",b);
+	
+	int temp = a;
+	a = b;
+	b = temp;
+	
+	printf("After\n");
+	printf("a (local copy): %d\n",a);
+	printf("b (local copy) : %d\n",b);
+	printf("=== LEAVING BROKEN SWAP ===\n");
+}
+
+void	working_swap(int *a, int *b )
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int main(){
+	int n = 42;
+	int m = 98;
+
+	printf("=== IN THE MAIN : ===\n");
+	printf("n = %d\n", n);
+	printf("m = %d\n", m);
+	printf("=== Hold on i'll swap with \tbroken one ===\n");
+	borken_swap(n,m);
+	printf("n = %d\n", n);
+	printf("m = %d\n", m);
+}
